e8324239ba86bb2d75c332441b8cdeb7eca49717
vinay <vinaymavi@gmail.com>
2022-04-14 11:58:23 +0530

    done

---
+++ python-learning/python-1.py
@@ -0,0 +1,16 @@
+name = input("What is your name? ")
+print("Hi " + name)
+print("Please rate us!")
+print ("Rate us in single word like: Good or bad")
+
+feedback = input("Rate me: ")
+
+if feedback == "bad":
+  print("sorry \U0001F612" )
+else:
+  feedback == "Good"
+  print("Thankyou \U0001F600")
+
+print("Thankyou for rating us! \U0001F600")
+
+
