092d2a12f8e6b729e94334a5e19a2fb44b19c11c
vinay <vinaymavi@gmail.com>
2022-06-09 11:20:43 +0530

    I have Learning

--- python-learning/python-1.py
+++ python-learning/python-1.py
@@ -1,16 +1,4 @@
-name = input("What is your name? ")
-print("Hi " + name)
-print("Please rate us!")
-print ("Rate us in single word like: Good or bad")
+First_name = "Vinay"
+Last_name = "Mavi"
 
-feedback = input("Rate me: ")
-
-if feedback == "bad":
-  print("sorry \U0001F612" )
-else:
-  feedback == "Good"
-  print("Thankyou \U0001F600")
-
-print("Thankyou for rating us! \U0001F600")
-
-
+print(f"{First_name} {Last_name} is a coder")
