f69d92441457d3566305a8f2a701ecdd34791639
maviaarav <76863907+maviaarav@users.noreply.github.com>
2025-01-12 18:31:11 +0530

    changed
    Changed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const nav = document.getElementById('navbar');
 const person = document.getElementById('button')
 const button = document.getElementById('btn-down');
@@ -22,3 +23,22 @@ button.addEventListener('click', () => {
     }
 
 });
+=======
+btn = document.getElementById('btn');
+nav = document.getElementById('nav');
+cross = document.getElementById('cross');
+
+function navbar() {
+    nav.style.height = '15vh';
+    nav.style.transition = '2s';
+    btn.style.display = 'none';
+    cross.style.display = 'block';
+}
+
+function up() {
+    nav.style.height = '0';
+    nav.style.transition = '2s';
+    cross.style.display = 'none';
+    btn.style.display = 'block';
+}
+>>>>>>> Stashed changes
