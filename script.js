c5ccad599108a546192711d02a82a4e006ab65c6
maviaarav <76863907+maviaarav@users.noreply.github.com>
2025-01-12 12:18:52 +0530

    changed

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-btn = document.getElementById('btn');
-nav = document.getElementById('nav');
-cross = document.getElementById('cross')
-function navbar(){
-    nav.style.height='15vh'
-    nav.style.transition='2s'
-    btn.style.display='none'
-    cross.style.display='block'
-}
-function up(){
-    nav.style.height='0'
-    nav.style.transition='2s'
-    cross.style.display='none'
-    btn.style.display='block'
-}
+const nav = document.getElementById('navbar');
+const person = document.getElementById('button')
+const button = document.getElementById('btn-down');
+
+// Add an event listener directly to the button
+button.addEventListener('click', () => {
+    nav.classList.toggle('hidden'); 
+    // button.style.display='none';
+    const navHeight = nav.offsetHeight;
+    if (navHeight== 0){
+        person.style.backgroundColor='Blue'
+        const imageElement = document.getElementById('myimage')
+        imageElement.src = 'cross.png'
+    }
+    else{
+        person.style.background= 'none'
+        person.style.boxShadow='none'
+        person.style.transition='1s'
+        const imageElement = document.getElementById('myimage')
+        imageElement.src = 'menu.png'
+    }
+
+});
