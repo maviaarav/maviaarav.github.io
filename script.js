59ac551a8781e6bafcb2a6c521d7c4c9b3db5ad7
maviaarav <76863907+maviaarav@users.noreply.github.com>
2025-01-14 19:14:12 +0530

    cheanged

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const button = document.getElementById('btn-down');
 button.addEventListener('click', () => {
     nav.classList.toggle('hidden');
     nav.style.display='flex' 
-    // button.style.display='none';
     const navHeight = nav.offsetHeight;
     if (navHeight== 0){
         person.style.backgroundColor='Blue'
@@ -22,20 +21,4 @@ button.addEventListener('click', () => {
     }
 
 });
-btn = document.getElementById('btn');
-nav = document.getElementById('nav');
-cross = document.getElementById('cross');
 
-function navbar() {
-    nav.style.height = '15vh';
-    nav.style.transition = '2s';
-    btn.style.display = 'none';
-    cross.style.display = 'block';
-}
-
-function up() {
-    nav.style.height = '0';
-    nav.style.transition = '2s';
-    cross.style.display = 'none';
-    btn.style.display = 'block';
-}
